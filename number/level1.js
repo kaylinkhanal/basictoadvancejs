22ba7baad86114a3b05d323f89a629ae311a949e
aswinbaral2 <aswinbaral2@gmail.com>
2025-12-03 07:35:58 +0545

    completed calculation task

--- number/level1.js
+++ number/level1.js
@@ -3,7 +3,13 @@ let b = 20
 
 
 //Q1. Calculate sum of a and b and store it in variable sum
+let sum = a + b
 
 //Q2. caluclate the power of a to b and store it in variable power
+let power = a ** b
 
 //Q3. Calculate the remainder when b is divided by a and store it in variable remainder
+let remainder = b % a
+console.log(sum)
+console.log(power)
+console.log(remainder)  
