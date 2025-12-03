818225fbea602b996291c47976fd965bef2ef3a0
Shyam Shah Thakuri <thakurishyam111@gmail.com>
2025-12-03 12:24:32 +0545

    level2.js

---
+++ number/level2.js
@@ -0,0 +1,24 @@
+//print the given number positive,negative and zero 
+let num = 10;
+if (num<0){
+    console.log('negative')
+}
+else if  (num>0){
+    console.log('positive')
+}
+else {
+    console.log('zero')
+}
+
+// Math.round(), Math.floor(), Math.ceil()
+
+let a=50.40
+console.log(Math.round(a))
+console.log(Math.floor(a))
+console.log(Math.ceil(a))
+
+
+let number =5
+for(let i=1;i<10;i++){
+    console.log(number*i)
+}
---
+++ number/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let n = 10;
+for (let i = 0; i <= n; i++) {
+  if (i% 2 ===! 0) {
+  
+
+  console.log(i);
