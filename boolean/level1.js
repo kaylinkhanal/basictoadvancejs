b39d07c7d21593ef4d56e0c3bd19d8ab2fb38656
Sona7x <sony.bajracharya22@gmail.com>
2025-12-03 22:33:37 +0545

    datatypes & operators practice

--- boolean/level1.js
+++ boolean/level1.js
@@ -1,9 +1,18 @@
-let a =10
-let b = 20
+let a = 10;
+let b = 20;
 
 // Q1. check if a is greater than b
-
+let c = a > b;
+console.log(c); //false
 
 /// Q2. check if  10 * a and check if it is greater or euqals than b
+let d = 10 * a >= b;
+console.log(d); //true
 
 // Q3. check if b is divisible by a
+let e = b % a === 0; // --- means if remainder is 0
+console.log(e); //true
+
+// Q4. check if d multiplied by 2 is equal to e
+let f = a + b == 30; // == means if its equal
+console.log(f);
---
+++ boolean/level2.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+// Q1. check if a is greater than b
+let d = a < b && c > a; // && means both must be true
+console.log(d); // true
