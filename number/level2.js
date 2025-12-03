6410c539163086efce7bb63bd38a8d18b7e9884b
Sagar Subedi <saagarsubedii999@gmail.com>
2025-12-03 23:10:01 +0545

    solved more question

---
+++ number/level2.js
@@ -0,0 +1,21 @@
+//calculate the area of the rectangle and store it in a variable named area.
+const length = 20  ;
+const breadth = 10  ;
+ let area = length * breadth ;
+ console.log("area of rectangle is " + area);
+
+ //use the increment operator (++) to increase its value by 1,
+ //  and then use the decrement operator (--) to decrease its value by 1. Print the value after both operations.
+ let value = 50 ;
+ let value2 = ++value ; //Pre-Increment)
+ console.log(value2);
+
+ let value6 = 45 ;
+ let value3 = --value6 ;  //Pre-Decrement)
+ console.log(value3);
+
+ let value4 = value++ ;  //(Post-Increment)
+ console.log(value4);
+
+ let value5 = value-- ;  //(Post-Increment)
+ console.log(value5);
---
+++ strings/level1.js
@@ -0,0 +1,7 @@
+//create a variable fullName that holds the value "Alex Johnson" (with a space in between).
+let firstName = "alex" ;
+let lastName = "johnsin" ;
+console.log( "my name is " + firstName +" " + lastName);
+
+//Check the data type of the variable
+console.log(typeof lastName);
