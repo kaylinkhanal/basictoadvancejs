8945f24cc636ba558ce82c42117df0299c7b02a4
Jagat05 <joshijagaths@gmail.com>
2025-12-03 07:31:41 +0545

    level 1 Number Assignment completed

--- number/level1.js
+++ number/level1.js
@@ -1,9 +1,14 @@
-let a = 10
-let b = 20
-
+let a = 10;
+let b = 20;
 
 //Q1. Calculate sum of a and b and store it in variable sum
+let sum = a + b;
+console.log("Sum", sum);
 
 //Q2. caluclate the power of a to b and store it in variable power
+let power = a ** b;
+console.log(power);
 
 //Q3. Calculate the remainder when b is divided by a and store it in variable remainder
+let remainder = b % a;
+console.log("Remainder", remainder);
