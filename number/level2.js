0228439d05823d63ef1a9eddea52fd91bc5c7423
SAGUN DANGOL <dangolsagun6@gmail.com>
2025-12-03 12:24:17 +0545

    level 2 number

---
+++ number/level2.js
@@ -0,0 +1,32 @@
+// print the age which is greater than 18 or not
+let age = 9;
+if(age>=18)
+{
+    console.log('YES')
+}
+else 
+{
+    console.log('NO')
+}
+
+// print the given number is positive, negative or zero.
+let num = 20;
+if(num>0)
+{
+    console.log('Positive')
+} 
+else if(num<0)
+{
+    console.log('Negative')
+}
+else
+{
+    console.log('Zero')
+}
+
+// print the table by taking input from user
+let n = 3;
+for(let i=1;i<=10;i++)
+{
+    console.log(n*i)    
+}
