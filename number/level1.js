4d9223e7a32ff620d27651f4acf267384dba3c8b
SAGUN DANGOL <dangolsagun6@gmail.com>
2025-12-03 07:36:14 +0545

    solve the answer

--- number/level1.js
+++ number/level1.js
@@ -3,7 +3,13 @@ let b = 20
 
 
 //Q1. Calculate sum of a and b and store it in variable sum
+let sum = a + b;
+console.log('Sum:', sum)
 
 //Q2. caluclate the power of a to b and store it in variable power
+let power = a ** b;
+console.log('Power:',  power)
 
 //Q3. Calculate the remainder when b is divided by a and store it in variable remainder
+let remainder = b % a;
+console.log('Remainder:', remainder)
