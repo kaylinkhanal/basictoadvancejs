d302c741e6480f3d3728fb6429203833343472b3
kaylinkhanal10 <kyalinnop@gmail.com>
2025-12-03 07:26:36 +0545

    Added level 1 number js questions

---
+++ number/level1.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = 20
+
+
+//Q1. Calculate sum of a and b and store it in variable sum
+
+//Q2. caluclate the power of a to b and store it in variable power
+
+//Q3. Calculate the remainder when b is divided by a and store it in variable remainder
