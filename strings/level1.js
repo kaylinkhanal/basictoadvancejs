722dec297b8e8b8c975ded5d89b0a6a5695e4148
Sona7x <sony.bajracharya22@gmail.com>
2025-12-03 22:33:37 +0545

    datatypes & operators practice

---
+++ strings/level1.js
@@ -0,0 +1,11 @@
+let userName = "ram";
+let age = "28";
+let address = "swoyambhu";
+
+// get the length of the name string
+let length = userName.length;
+console.log(length);
+
+// City to uppercase
+let uppercase = address.toUpperCase();
+console.log(uppercase);
---
+++ strings/level2.js
@@ -0,0 +1,7 @@
+let age = "28";
+
+// first character of age
+let firstcharacter = age[0];
+let lastcharacter = age[age.length - 1];
+let totalOfCharacter = Number(firstcharacter) + Number(lastcharacter);
+console.log(totalOfCharacter);
