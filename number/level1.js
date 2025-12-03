3ee8a5826352d9d7e6d40e56b5c76d7ffb15d4e0
Jagat05 <joshijagaths@gmail.com>
2025-12-03 19:31:54 +0545

    Number Level 1- Super Easy

--- number/level1.js
+++ number/level1.js
@@ -1,5 +1,6 @@
 let a = 10;
 let b = 20;
+let c = 3;
 
 //Q1. Calculate sum of a and b and store it in variable sum
 let sum = a + b;
@@ -19,3 +20,30 @@ console.log(addTen);
 //Q5. Find the half value of a
 let halfValue = a / 2;
 console.log(halfValue);
+
+// Q6. Add a and b, then multiply the result by c. Store in variable result1
+let result1 = (a + b) * c;
+console.log(result1);
+
+// Q7. Calculate a squared plus b squared. Store in variable squares
+let squares = a ** 2 + b ** 2;
+console.log(squares);
+
+//Q8. Find the average of a, b, and c. Store in variable average
+let average = (a + b + c) / 3;
+console.log(average);
+
+//Q9. Multiply a and b, then find the remainder when divided by c. Store in variable remValue
+remValue = (a * b) % c;
+console.log(remvalue);
+
+// Q10. Subtract b from a, and then raise the result to the power of c. Store in variable powerValue
+let powerValue = (b - a) ** c;
+
+// Q11. Calculate the sum of a, b, and c, then divide by b. Store in variable result2
+let result2 = (a + b + c) / b;
+console.log(result2);
+
+// Q12. Find the product of a and c, then subtract b. Store in variable result3
+let result3 = a * c - b;
+console.log(result3);
