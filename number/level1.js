c9de6f24b9c97f20a8b2a13dbddc27b0cb26cc1b
Jagat05 <joshijagaths@gmail.com>
2025-12-03 19:23:20 +0545

    Number Level 1- Super Easy

--- number/level1.js
+++ number/level1.js
@@ -12,3 +12,10 @@ console.log(power);
 //Q3. Calculate the remainder when b is divided by a and store it in variable remainder
 let remainder = b % a;
 console.log("Remainder", remainder);
+//Q4. Add 10 to a and store it in variable addTen
+let addTen = a + 10;
+console.log(addTen);
+
+//Q5. Find the half value of a
+let halfValue = a / 2;
+console.log(halfValue);
