5c4704d38cd43e69a91b2c37773f06ef6ae12c10
dharmendra <[karkidharmendra21@gmail.com]>
2025-12-04 21:48:48 +0545

    Boolean Lvl1 Completed

--- boolean/level1.js
+++ boolean/level1.js
@@ -2,8 +2,37 @@ let a =10
 let b = 20
 
 // Q1. check if a is greater than b
+ console.log(a>b);
 
 
 /// Q2. check if  10 * a and check if it is greater or euqals than b
+console.log(a*10>=b);
+
 
 // Q3. check if b is divisible by a
+console.log((b%a)===0);
+
+
+
+// Q4. Check if age = 15 is allowed to vote (age ≥ 18).
+let age= 15;
+ if (age>=18) 
+    {
+        console.log("you can vote");
+    
+ }
+ console.log("you can't vote");
+    
+
+// Q5. Check if either a = 9 or b = 4 is greater than 7.
+
+let A=9;
+let B=4;
+console.log(A>7,B>7);
+
+
+
+
+
+
+ 
