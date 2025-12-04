9c7b5ee5dac50ff64029978331cd635bec3a775a
Sona7x <sony.bajracharya22@gmail.com>
2025-12-04 08:21:18 +0545

    array level1.js solve

--- array/level1.js
+++ array/level1.js
@@ -1,17 +1,28 @@
-// Array 
+// Array
 // ---> [4,5,'3', null, undefined, true, false]]
 // based on index
 // [ ] big bracket, separated by comma
 
-let ramScore = [95, 92, 87]
-let shyamScore = [77,99,99]
+let ramScore = [95, 92, 87];
+let shyamScore = [77, 99, 99];
 
 // Q1. calcuate the length of array
-
+console.log(ramScore.length);
+console.log(shyamScore.length);
 
 // Q2. get the output of last element of array and check if it is divisible by 21
+let lastArrayRam = ramScore[ramScore.length - 1] % 21 === 0;
+console.log(lastArrayRam);
 
+let lastArrayShyam = shyamScore[shyamScore.length - 1] % 21 === 0;
+console.log(lastArrayShyam);
 
 // Q3. check if ram percentage is greater than shyam percentage
+let ramPercentage = ((ramScore[0] + ramScore[1] + ramScore[2]) / 300) * 100;
+console.log(ramPercentage);
 
+let shyamPercentage =
+  ((shyamScore[0] + shyamScore[1] + shyamScore[2]) / 300) * 100;
+console.log(shyamPercentage);
 
+console.log(ramPercentage > shyamPercentage);
