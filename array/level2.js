7370bdf97df838484c71a472bc6d638eaf34fc8b
dharmendra <[karkidharmendra21@gmail.com]>
2025-12-04 15:37:20 +0545

    Level 1 number completed

--- array/level2.js
+++ array/level2.js
@@ -4,13 +4,9 @@ const arr2 = [6,2,3,1,5]
 
 
 //Q1.  Output should be [3,4,5,6,1,6,2,3,1,5]
-const mergearray=arr.concat(arr2)
-console.log(mergearray)
 
 
 //Q2. output shoulb be [5,1,3,2,6]
-const reversed=arr2.reverse()
-console.log(reversed)
 
 
 
