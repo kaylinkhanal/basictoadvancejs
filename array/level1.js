f311a3c6443efce02730b9fb1d248b9070467c26
Sagar Subedi <saagarsubedii999@gmail.com>
2025-12-04 09:36:45 +0545

    class question solved

--- array/level1.js
+++ array/level1.js
@@ -7,11 +7,20 @@ let ramScore = [95, 92, 87]
 let shyamScore = [77,99,99]
 
 // Q1. calcuate the length of array
+let length = ramScore.length;
+let length2 = shyamScore.length;
+console.log(length,length2);
 
 
 // Q2. get the output of last element of array and check if it is divisible by 21
+let lastElement = (ramScore[ramScore.length -1] % 21 === 0);
+console.log(lastElement);
 
 
 // Q3. check if ram percentage is greater than shyam percentage
+let ramPercentage = ( 95 + 92 + 87)/300 * 100;
+let shyamPercentage = ( 77 + 92 +87)/300 * 100;
+let result = ramPercentage > shyamPercentage;
+console.log(result);
 
 
--- array/level2.js
+++ array/level2.js
@@ -4,9 +4,14 @@ const arr2 = [6,2,3,1,5]
 
 
 //Q1.  Output should be [3,4,5,6,1,6,2,3,1,5]
+let output = arr.concat(arr2);
+console.log(output);
 
 
 //Q2. output shoulb be [5,1,3,2,6]
+let output2 = arr2(reverse);
+console.log(output2);
+
 
 
 // hw: map vs foreach vs filter  in array
