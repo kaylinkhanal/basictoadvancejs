89d660c963d006ec3928484ad1a6e8bd01d70df7
Jagat05 <joshijagaths@gmail.com>
2025-12-04 08:20:37 +0545

    Level 1 COmpleted

--- array/level1.js
+++ array/level1.js
@@ -17,3 +17,14 @@ console.log(ramScore[ramScore.length - 1] % 21 === 0);
 let ramPercentage = (ramScore[0] + ramScore[1] + ramScore[2]) / 3;
 let shyamPercentage = (shyamScore[0] + shyamScore[1] + shyamScore[2]) / 3;
 console.log(ramPercentage > shyamPercentage);
+
+// Q4. Compare first and last element
+console.log(ramScore[0] === ramScore[ramScore.length - 1]);
+
+// Q5. Check if array contains number 87
+console.log(ramScore.includes(87));
+
+// Q6. Check the sum of last two score of ram is greater than shyam's first score
+console.log(
+  ramScore[ramScore.length - 1] + ramScore[ramScore.length - 2] > shyamScore[0]
+);
