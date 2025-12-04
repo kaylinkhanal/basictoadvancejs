5e5751e028686f560231f7013d9ba47e1c4af101
dharmendra <[karkidharmendra21@gmail.com]>
2025-12-04 22:10:53 +0545

    String level1&level2 solved

---
+++ strings/level1.js
@@ -0,0 +1,32 @@
+// Q1. Check if two strings are equal
+let name1 = "dharmendra";
+let name2 = "dharmendra";
+
+console.log(name1 === name2);
+
+
+// Q2. Check if a string contains a given word
+let sentence = "I am learning JavaScript";
+console.log(sentence.includes("JavaScript"));
+
+
+
+// Q3. Check if email contains '@'
+let email = "hello@gmail.com";
+
+console.log(email.includes("@"));
+
+
+// Q4. Check if two strings have the same length
+let a = "cat";
+let b = "dog";
+
+console.log(a.length === b.length);
+
+
+
+
+// Q10. Check if a string is empty
+let emptyStr = "";
+
+console.log(emptyStr.length === 0);
