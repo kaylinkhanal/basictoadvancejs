d3a5e097ca3433550b9b0109161c56d72d6f6493
aswinbaral2 <aswinbaral2@gmail.com>
2025-12-05 00:15:14 +0545

    map vs for each vs filter

--- Task/task2.js
+++ Task/task2.js
@@ -1,4 +1,27 @@
-/**********************************************************
-Q1. Write a code that selects random months from an array 
-of all the months in a year and outputs them in a new array. 
-***********************************************************/
+// hw: map vs foreach vs filter  in array
+/***********************************************
+    * 1!. Extract Ages & Create a New Array of Ages---> use of map()
+ ***********************************************/
+const users = [
+    { name: "Alice", age: 20 },
+    { name: "Bob", age: 25 },
+    { name: "Charlie", age: 30 }
+];
+const userAge = users.map(user => user.age)
+console.log(userAge);// Output: [20, 25, 30]
+/***********************************************
+    * 2. Filter Even Numbers from an Array---> use of filter()
+***********************************************/
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers.filter(num => num % 2 === 0)
+console.log(evenNumbers)//[ 2, 4, 6, 8, 10 ]
+/***********************************************/
+// . Log Each Element with forEach ---> use of forEach()
+/***********************************************/
+const fruits = ["apple", "banana", "cherry", "date"];
+fruits.forEach(fruit => console.log(fruit));
+// Output:
+// apple
+// banana
+// cherry
+// date
