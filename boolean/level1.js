6fccaa3d84638cc6cf2b965512922a275be2afc0
Jagat05 <joshijagaths@gmail.com>
2025-12-04 05:58:38 +0545

    Boolean Lvl1 Completed

--- boolean/level1.js
+++ boolean/level1.js
@@ -1,8 +1,22 @@
 let a = 10;
 let b = 20;
+let c = 40;
 
 // Q1. check if a is greater than b
+console.log(a > b);
 
 /// Q2. check if  10 * a and check if it is greater or euqals than b
+console.log(10 * a >= b);
 
 // Q3. check if b is divisible by a
+console.log(b % a === 0);
+
+// Q4. Check if b is equal to 20.
+console.log(b === 20);
+
+// Q5. Check if a + b is greater than c
+console.log(a + b > c);
+
+// Q6. Check if score is between 50 and 100.
+let score = 75;
+console.log(score > 50 && score < 100);
