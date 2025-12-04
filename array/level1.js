c79871d79d694181ea2ca91eac233c5491e8ac16
dharmendra <[karkidharmendra21@gmail.com]>
2025-12-04 22:25:44 +0545

    array Lvl1 completed

--- array/level1.js
+++ array/level1.js
@@ -6,10 +6,14 @@
 let ramScore = [95, 92, 87]
 let shyamScore = [77,99,99]
 
+
 // Q1. calcuate the length of array
+console.log(ramScore.length);
+
 
 
 // Q2. get the output of last element of array and check if it is divisible by 2
+console.log(ramScore[ramScore.length - 1] % 21 === 0);
 
 
 
@@ -17,12 +21,20 @@ let shyamScore = [77,99,99]
 
 
 
+//Q4. Print the first element of each array
+
+console.log(ramScore[0]);  
+console.log(shyamScore[0]);
+
+
+//Q5. Replace the 2nd element in Ram’s array with 100
+
+ramScore[1] = 100;
+console.log(ramScore);
 
 
 
 
 
-
-//mapvs foreach vs filter in array
 
 
