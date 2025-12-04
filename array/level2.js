bb8fc6643f0e5b09d12b0944adccacc04c9f531c
yogeshthapa-7 <yogsthapa@gmail.com>
2025-12-04 08:23:32 +0545

    answers added to level2 array

--- array/level2.js
+++ array/level2.js
@@ -4,9 +4,13 @@ const arr2 = [6,2,3,1,5]
 
 
 //Q1.  Output should be [3,4,5,6,1,6,2,3,1,5]
+console.log(arr.concat(arr2));
 
 
 //Q2. output shoulb be [5,1,3,2,6]
+let reverse = arr2.reverse();
+console.log(reverse);
+
 
 
 // hw: map vs foreach vs filter  in array
