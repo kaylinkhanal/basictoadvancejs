2191747cddfa0c75ffa3c9b9710a2ad2fb3601f4
Prakash Thapa <bluestar5pt4488@gmail.com>
2025-12-04 09:21:40 +0545

    solution completed

--- array/level1.js
+++ array/level1.js
@@ -5,7 +5,7 @@
 
 let ramScore = [95, 92, 87]
 let shyamScore = [77,99,99]
-
+/*
 // Q1. calcuate the length of array
 
 console.log(ramScore.length)
@@ -14,12 +14,23 @@ console.log(shyamScore.length)
 
 // Q2. get the output of last element of array and check if it is divisible by 21
 
-// console.log({get(ramScore; length(ramScore) - 2)})
-console.log(shyamScore(length(shyamScore)-2)/21)
+let lastElement = ramScore[ramScore.length - 1];
+console.log("Last element:", lastElement);
+
+// console.log(ramScore[ramScore.length - 1])
+
+if (lastElement % 21 === 0) {
+    console.log("divisible");
+} else {
+    console.log("NOT divisible");
+}
+*/
+
 
 // Q3. check if ram percentage is greater than shyam percentage
 
-// let ramper = (ram[95]+ram[92]+ram[87])/300*100;
-// let shyamper = (ram[77]+ram[99]+ram[99])/300*100;
+const a = (ramScore[0]+ramScore[1]+ramScore[2])/300 * 100;    //ramScore[initial index]+ramScore[next index]+..
+const b = (shyamScore[0]+shyamScore[1]+shyamScore[2])/300 * 100;
 
-// console.log(ramper>shyamper)
+console.log(a > b)
+
