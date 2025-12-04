e01ecaaebdda14830ee45e8b032a2a8505db5a3b
Jagat05 <joshijagaths@gmail.com>
2025-12-04 06:37:55 +0545

    Boolean lvl1 Solved

---
+++ strings/level1.js
@@ -0,0 +1,46 @@
+// Q1. Check if two strings are equal
+let name1 = "Jagat";
+let name2 = "Jagat";
+
+console.log(name1 === name2);
+
+// Q2. Check if a string contains a given word
+let sentence = "I am learning JavaScript";
+console.log(sentence.includes("JavaScript"));
+
+// Q3. Check if string starts with a capital letter
+let word = "Hello";
+console.log(word[0] === word[0].toUpperCase());
+
+// Q4. Check if the length of a string is greater than 5
+let city = "Kathmandu";
+console.log(city.length > 5);
+
+// Q5. Check if the last character of a string is '!'
+let msg = "Welcome!";
+console.log(msg.endsWith("!"));
+
+// Q6. Check if a username contains spaces
+let username = "jagat joshi";
+console.log(username.includes(" "));
+
+// Q7. Check if email contains '@'
+let email = "hello@gmail.com";
+
+console.log(email.includes("@"));
+
+// Q8. Check if two strings have the same length
+let a = "cat";
+let b = "dog";
+
+console.log(a.length === b.length);
+
+// Q9. Convert a string to lowercase and check if it equals "admin"
+let input = "ADMIN";
+
+console.log(input.toLowerCase() === "admin");
+
+// Q10. Check if a string is empty
+let emptyStr = "";
+
+console.log(emptyStr.length === 0);
