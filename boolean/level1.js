bb86fbfc8a30e85ba65a2fe61b9b919a50013320
alishkdk <kdkalish4@gmail.com>
2025-12-05 06:46:52 +0545

    slove

--- boolean/level1.js
+++ boolean/level1.js
@@ -2,8 +2,13 @@ let a =10
 let b = 20
 
 // Q1. check if a is greater than b
-
+let output =a>b;
+console.log(output)
 
 /// Q2. check if  10 * a and check if it is greater or euqals than b
+let sum=a*10;
+let check=sum>b;
+console.log(check)
 
 // Q3. check if b is divisible by a
+
