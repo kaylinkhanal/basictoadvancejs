50b3896ad7dd282b276b8d990e5afd9182338af2
kaylinkhanal10 <kyalinnop@gmail.com>
2025-12-07 08:14:10 +0545

    added arr level5

---
+++ array/level5.js
@@ -0,0 +1,18 @@
+const arr = [5,1,4]
+
+// syntax for map 
+
+// const mappedArr = arr.map((item) => {
+//     return element * 2
+// })
+//console.log(mappedArr); // [10, 2, 8]
+//--------------------
+
+
+
+//Q1. if item is divisible by 2, then multiply it by 3 else multiply it by 2
+// expected output: [10, 2, 12]
+
+
+//Q2. use filter to get only those items which are greater than 2
+// expected output: [5,4]
