b3e8474df3af5f68d969586993052f685dc0146a
Birendra Bohara <bbirendra693@gmail.com>
2025-12-07 08:21:32 +0545

    boolean level 1 practice

--- boolean/level1.js
+++ boolean/level1.js
@@ -2,10 +2,10 @@ let a =10
 let b = 20
 
 // Q1. check if a is greater than b
-console.log(a>b)
+console.log(a>b) // false
 
 /// Q2. check if  10 * a and check if it is greater or euqals than b
-console.log(10*a>=b)
+console.log(10*a>=b) // true
 
 // Q3. check if b is divisible by a
-console.log(b%a===0)
+console.log(b%a===0) // true
