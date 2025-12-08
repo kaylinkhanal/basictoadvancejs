d80cd3902685af464c65d7788251b6eb3dc5c562
kaylinkhanal10 <kyalinnop@gmail.com>
2025-12-08 07:21:12 +0545

    add loop array questions

---
+++ array/level9.js
@@ -0,0 +1,15 @@
+const arr = [5,1,2,4]
+
+//Q1. use map to multiply each item by 2 if even and remove the odds ones
+
+
+
+
+const output = arr.map((item) => {
+
+}).filter((item) => {
+    return item
+});
+
+
+// Q2 use foreach to calculate the sum of all even numbers in the array
