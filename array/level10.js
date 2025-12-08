584d5fd8c0940d03c72f0350360824a4e70ba7a8
kaylinkhanal10 <kyalinnop@gmail.com>
2025-12-08 08:06:11 +0545

    add loop array level 10

---
+++ array/level10.js
@@ -0,0 +1,18 @@
+const arr = ['ram',90,70, 50]
+const arr2 = ['shyam', 99,88,35]
+
+// Q1. create an array which contains names of students only
+
+// hint use typeof operator
+const names = arr.concat(arr2).filter((item) => {
+    // yedi item to typeof 'string' ho vaney matra return garne
+});
+
+
+// Q2. calculate the total score of ram and shyam 
+
+let sum = 0
+([...arr,...arr2]).forEach((item) => {
+    // yedi item typeof 'number' ho vaney sum ma add garne
+});
+console.log(sum)
