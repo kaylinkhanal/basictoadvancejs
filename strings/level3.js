83f71bbc07519c829e243509dafcd1a35ce47198
yogendrabohara <yogendrabohara887>
2025-12-08 07:50:15 +0545

    array

---
+++ strings/level3.js
@@ -0,0 +1,46 @@
+// variables(let,const var)
+// store your name
+let name = "yogendra";
+const age = 20;
+console.log(name);
+
+// store three numbers 
+let num1 = 10 ;
+let num2 = 20 ;
+let num3 = 30 ;
+console.log(num1,num2,num3);
+
+// change a variable value 
+let age1 = 18 ;
+age1 = 20 ;
+console.log(age1);
+
+// practice
+let student = {
+    name:"Ram",
+    age: 18,
+    passed: true
+};
+console.log(student.passed);
+
+// operators
+// add two numbers using + operators
+let a = 5;
+let b = 7;
+let sum = a + b;
+console.log(sum);
+
+// check if age > 18
+let age2 = 22;
+if (age2 > 18 ) {
+console.log("You are adult");
+} else {
+    console.log("You are not adult");
+}
+
+// find remainder using  modulus (%)
+let x = 10;
+let y = 3;
+let remainder = x % y;
+ console.log(remainder);
+
