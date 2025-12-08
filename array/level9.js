b8a3685fae86e817bb97556a403ace871723a34d
aswinbaral2 <aswinbaral2@gmail.com>
2025-12-08 08:07:44 +0545

    level 9 solved in class

--- array/level9.js
+++ array/level9.js
@@ -1,15 +1,19 @@
 const arr = [5,1,2,4]
 
-//Q1. use map to multiply each item by 2 if even and remove the odds ones
+//Q1. use map to multiply each item by 3 if even and remove the odds ones
 
+const arrMap = arr.map(item => item%2 === 0 ? item * 3 : item ).filter(item => (item % 2 === 0) )
+console.log(arrMap);//[ 15, 6, 12 ]
 
-
-
-const output = arr.map((item) => {
-
-}).filter((item) => {
-    return item
-});
-
+//-----------------------------------------------------------------------
 
 // Q2 use foreach to calculate the sum of all even numbers in the array
+let sumEven = 0;
+const arrEach = arr.forEach(num => {
+    if(num % 2 === 0) {
+        sumEven += num;
+    }
+});
+console.log(sumEven);
+//Output: 6
+//-----------------------------------------------------------------------
