5afb2cd1bb4d434296f852ff41a259210b66a921
Birendra Bohara <bbirendra693@gmail.com>
2025-12-08 08:29:26 +0545

    array level 3 practice

--- array/level3.js
+++ array/level3.js
@@ -26,3 +26,29 @@ const arr = [5,1,4]
 // expected output: [5,4]
 let filterOut = arr.filter(item => item > 2);
 console.log(filterOut);
+
+
+const arr1 = [5,1,2,4]
+//Q1. use map to multiply each item by 3 if even and remove the odds ones
+const output1 = arr1.map((item) => {
+    if(item%2===0){
+    return item*3;
+    }
+}).filter((item) => {
+    // if(item%2!==0){
+    //     return item;
+    // }
+    return item;
+    
+})
+console.log(output1);
+
+
+// Q2 use foreach to calculate the sum of all even numbers in the array
+    let sum = 0
+arr1.forEach((item) => {
+    if(item %2 ===0){
+        sum += item
+    }
+});
+console.log(sum)
