57e0cb4d85c1c7f8f4793c627f81b825dc312ebf
Bishmeeta <jhalakkumarithapa520@gmail.com>
2025-12-08 18:30:49 +0545

    added more

--- array/level5.js
+++ array/level5.js
@@ -64,11 +64,62 @@ console.log(doubledArrayArrow);
  console.log(longnames);
 
  //Q8.convert given array to capital 
- const input=['ram','bishmeeta','aloj'];
+ const input=['ram','shyam','hary'];
  const uppercase=input.map((item)=>{
     return item.toUpperCase();
  })
  console.log(uppercase);
+// Q9.Use map to create a new array where every number in the numbers array is doubled..
+const number = [2, 5, 10, 3];
+const double= number.map(n=>n*2);
+console.log(double);
+
+//Q10. create a new array where every word is converted to uppercase.
+const words= ['one', 'two', 'three'];
+/*const upper=words.map((item)=>{
+    return item.toUpperCase();
+})
+console.log(upper);*/
+
+const upper=words.map(words=> words.toUpperCase());
+console.log(upper);
+
+//Q11.use map to create a new array where each object is modified to include a new boolean property called isActive and set its value to true.
+const user = [
+    { name: 'Alice', role: 'Dev' },
+    { name: 'Bob', role: 'Tester' }
+];
+const array= user.map(obj=>{
+    return {...obj, isActive: true};
+});
+console.log(array)
+
+
+// Q12.Use map to create a new array where each number is converted into a string prefixed with the dollar sign$.
+ numbers= [1, 2, 3, 4, 5]
+ const num =numbers.map(number => `$${number}`);
+ console.log(num)
+
+//Q13.use filter to create a new array containing only the ages that are 18 or older (eligible to vote).
+ages = [12, 18, 25, 8, 30];
+const older = ages.filter(checkadult);
+function checkadult(age)
+{
+    return age>=18;
+}
+console.log(older);
+
+// Q14.use filter to create a new array containing only the products whose price is less than 10.
+const products = [
+    { name: 'A', price: 5 },
+    { name: 'B', price: 12 },
+    { name: 'C', price: 8 }
+];
+const affordableproduct=products.filter(products=> products.price<10);
+console.log(affordableproduct);
+
+
+
 
 
  
