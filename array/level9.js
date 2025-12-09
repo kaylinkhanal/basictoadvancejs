012c5a8b24c02f370928cf4e9c2de415171a82bf
yogeshthapa-7 <yogsthapa@gmail.com>
2025-12-09 09:37:24 +0545

    new codes added to array level9

--- array/level9.js
+++ array/level9.js
@@ -1,7 +1,6 @@
 const arr = [5,1,2,4]
 
 //Q1. use map to multiply each item by 3 if even and remove the odds ones
-<<<<<<< HEAD
 
 const output = arr.map((item) => {
 
@@ -10,19 +9,12 @@ const output = arr.map((item) => {
     }
 
 
-=======
-
-
-const output = arr.map((item) => {
-    if(item %2 ===0){
-        return item * 3
-    }
->>>>>>> 584d5fd8c0940d03c72f0350360824a4e70ba7a8
 }).filter((item) => {
     
     return item;
 });
 console.log(output);
+
 
 // Q2 use foreach to calculate the sum of all even numbers in the array
 
@@ -33,3 +25,51 @@ arr.forEach((item) => {
     }
 });
 console.log(sum);
+
+
+// Q3 use filter to create a new array which contains only odd numbers from the array
+const newarr = arr.filter((item) => {
+  if(item %2 !==0)
+    return item;
+});
+console.log(newarr);
+
+
+// Q4 use map to create a new array which contains each number plus its index
+arr.map((value, index) => {
+  console.log(index,value);
+});
+
+
+
+// Q5 use foreach to print each item with its index in the format "index: item"
+arr.forEach((value, index) => {
+  console.log(index + ":" + value);
+});
+
+
+// Q6 use filter to create a new array which contains numbers greater than 2
+const smallerNumber = arr.filter((item) => {
+if (item <= 2){
+    return item;
+}
+});
+console.log(smallerNumber);
+
+
+// Q7 use map to create a new array which contains square of each number
+const square = arr.map((item) => {
+    return item ** 2;
+});
+console.log(square);
+
+
+// Q8 use foreach to calculate the product of all numbers in the array
+let product =1;      // initial value 1 because multiplying with 1 will not change the result
+arr.forEach((item) => {
+ product *= item;
+});
+console.log(product);
+
+
+
