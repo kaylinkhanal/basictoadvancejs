497840427ff7b88e9cc89814fac826d0192ab53b
aswinbaral2 <aswinbaral2@gmail.com>
2025-12-10 08:36:43 +0545

    class assignment

--- array/level11.js
+++ array/level11.js
@@ -9,7 +9,8 @@
 
 // const arr = [-1,4,-5,1,-2,4]
 //Q1. calculate the sum of all -ve numbers in the array
-// const output = arr.reduce((sum,nextItem,nextid,orgArr)=>{
- 
-// })
+
+const arr = [-1,4,-5,1,-2,4];
+const output = arr.reduce((a,b)=> b<0 ?  a+b: a);
+console.log(output)
 
