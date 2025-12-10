e44bf4bb4c0e3ce0bdf2d3d4e9c7a31913e47097
Sagar Subedi <saagarsubedii999@gmail.com>
2025-12-10 16:05:37 +0545

    solved

--- array/level10.js
+++ array/level10.js
@@ -1,4 +1,4 @@
-const arr = ['ram',90,70, 50]
+const arr = ['ram','hari',90,70, 50]
 const arr2 = ['shyam', 99,88,35]
 
 // Q1. create an array which contains names of students only
--- array/level11.js
+++ array/level11.js
@@ -7,9 +7,14 @@
 // -> loops might run less number of times than forEach and map
 // ->
 
-// const arr = [-1,4,-5,1,-2,4]
+const arr = [-1,4,-5,1,-2,4]
 //Q1. calculate the sum of all -ve numbers in the array
-// const output = arr.reduce((sum,nextItem,nextid,orgArr)=>{
+ const output = arr.reduce((sum,nextItem,nextid,orgArr)=>{
+    if(nextItem < 0){
+         sum = sum + nextItem
+    }
+    return sum
  
-// })
+ },0)
+ console.log(output);
 
