93eeeb347bf628cabc6f6bb57f0d8584956f5ba1
Jagat05 <joshijagaths@gmail.com>
2025-12-10 06:21:29 +0545

    Reduce Method

--- array/level10.js
+++ array/level10.js
@@ -24,3 +24,42 @@ let sum = 0;
   }
 });
 console.log(sum);
+
+//reduce
+//-->reduces (combines) the whole array into one final value.
+
+//syntax
+/*array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, initialValue);
+*/
+
+// Sum of Array//--Expected Result 60
+const arr4 = [10, 20, 30];
+const total = arr4.reduce((acc, curr) => acc + curr, 0);
+console.log(total);
+
+//Multiply All Numbers--Expected Result 24
+const arr5 = [2, 3, 4];
+const product = arr5.reduce((acc, curr) => acc * curr, 1);
+console.log(product);
+
+// Find Maximum Value
+// Expected Result 90
+const arr6 = [11, 25, 8, 90, 44];
+const max = arr6.reduce((acc, curr) => Math.max(acc, curr));
+console.log(max); // 90
+
+// Count How Many Students Passed
+// Expected Result 2
+const students = [
+  { name: "Ram", marks: 85 },
+  { name: "Shyam", marks: 45 },
+  { name: "Sita", marks: 92 },
+];
+
+const passed = students.reduce((acc, s) => {
+  if (s.marks >= 50) acc++;
+  return acc;
+}, 0);
+console.log(passed);
--- array/level9.js
+++ array/level9.js
@@ -1,7 +1,6 @@
 const arr = [5, 1, 2, 4];
 
 //Q1. use map to multiply each item by 3 if even and remove the odds ones
-<<<<<<< HEAD
 
 const output = arr
   .map((item) => {
@@ -17,25 +16,3 @@ arr.forEach((item) => {
   sum = sum + item;
 });
 console.log(sum);
-=======
-
-
-const output = arr.map((item) => {
-    if(item %2 ===0){
-        return item * 3
-    }
-}).filter((item) => {
-    return item
-});
-
-console.log(output)
-// Q2 use foreach to calculate the sum of all even numbers in the array
-
-let sum = 0
-arr.forEach((item) => {
-    if(item %2 ===0){
-        sum += item
-    }
-});
-console.log(sum)
->>>>>>> 584d5fd8c0940d03c72f0350360824a4e70ba7a8
