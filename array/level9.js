b0ee7c009b789105f7994b4b0c08911f9159348f
Sagar Subedi <saagarsubedii999@gmail.com>
2025-12-10 08:12:29 +0545

    solved

--- array/level9.js
+++ array/level9.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 /*const output = arr.map((item) => {
 
-=======
-const arr = [5,1,2,4]
-
-//Q1. use map to multiply each item by 3 if even and remove the odds ones
-
-
-const output = arr.map((item) => {
-    if(item %2 ===0){
-        return item * 3
-    }
->>>>>>> 584d5fd8c0940d03c72f0350360824a4e70ba7a8
 }).filter((item) => {
     return item
 });*/
@@ -30,7 +18,6 @@ console.log(array2);
 console.log(output)
 // Q2 use foreach to calculate the sum of all even numbers in the array
 
-<<<<<<< HEAD
 
 
 
@@ -42,12 +29,3 @@ const array3 = arr.forEach((item)=>{
         
 })
 console.log(sum);
-=======
-let sum = 0
-arr.forEach((item) => {
-    if(item %2 ===0){
-        sum += item
-    }
-});
-console.log(sum)
->>>>>>> 584d5fd8c0940d03c72f0350360824a4e70ba7a8
