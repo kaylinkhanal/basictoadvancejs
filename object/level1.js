d24299dbbe0e4ccd10e758219fd8a84b139a23f3
hritikbk07 <hritikkami56@gmail.com>
2025-12-11 08:13:01 +0545

    obj level1

--- object/level1.js
+++ object/level1.js
@@ -7,16 +7,20 @@ const obj = {
 }
 
 // Q1. output should be trigonometry
-
+console.log(obj.topics[2]);
 
 //Q2. output should be 3 (number of topics)
+console.log(obj.topics.length);
 
 
 //Q3. add one level to the current level in the object
 //expected output : level: 2
  // ---> write your code here
-// console.log(obj.level)
+ obj.level=obj.level+1
+console.log(obj.level);
 
 
 
 //Q4. delete subject from the object
+delete obj.subject;
+console.log(obj);
