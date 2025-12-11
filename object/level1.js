084eb300532f7005283d9700f9e6df3e7de4e49c
alishkdk <kdkalish4@gmail.com>
2025-12-11 07:54:07 +0545

    add

---
+++ object/level1.js
@@ -0,0 +1,17 @@
+const person={
+    firstname:'Aloj',
+    lastname:'kahdka',
+    address:'lalbandi'
+
+}
+console.log(person.firstname+"  "+person.lastname)
+
+//Add 
+
+const ob={
+    name:'aloj',
+    contact:98777,
+
+}
+ob.place='lalbandi';
+console.log(ob)
