6cc94ef1e4c8ba514c766f6c0483e196f3d4b2d0
alishkdk <kdkalish4@gmail.com>
2025-12-12 07:10:27 +0545

    upto Question no 9 solve

--- object/level2.js
+++ object/level2.js
@@ -28,3 +28,26 @@ console.log(output2)
 const item = { name: "Pen", price: 20 };
 
 console.log('price' in item);
+
+//5. Merge two simple objects using spread operator {...obj1, ...obj2}.
+const std={
+    name:'Aloj',
+    class:12,
+    address:'lalbandi'
+}
+   const mrg={...obj,...std}
+   console.log(mrg)
+
+//Q6. Convert an object into an array of [key, value] using Object.entries().
+
+     console.log(Object.entries(std))
+
+
+//Q7. Create an object and print type using typeof.
+     console.log(typeof std)
+
+
+//Q8. Freeze an object and try to change its value
+Object.freeze(std)
+std.address="kathmandu"
+console.log(std.address)
