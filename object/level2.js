a7d5de3338c80e101fac6bc83a759472908575c5
hritikbk07 <hritikkami56@gmail.com>
2025-12-12 07:12:01 +0545

    here for object level2

--- object/level2.js
+++ object/level2.js
@@ -8,12 +8,17 @@ const obj = {
 }
 
 // Q1. // expected output : { math: 49, science: 40, computer: 40 }
-
+console.log(obj.scores);
 
 // Q2. expected output : 6 (calculate the total length of name)
+console.log(obj.name.length);
 
 
 //Q3.  expected output  : [49, 40, 40]
+const allValues= Object.values(obj.scores);
+console.log(allValues);
 
 
 //Q4. expected output: [ 'math', 'science', 'computer' ]
+const keysArray= Object.keys(obj.scores)
+console.log(keysArray);
