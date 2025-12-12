babaac5b38c60675c8a3d6dabd90ec7730f88b7e
Sagar Subedi <saagarsubedii999@gmail.com>
2025-12-12 07:22:12 +0545

    solved

--- array/level10.js
+++ array/level10.js
@@ -20,14 +20,7 @@ console.log(names)
 // Q2. calculate the total score of ram and shyam 
 
 let sum = 0
-<<<<<<< HEAD
 let array4 =([...arr,...arr2]).forEach((item) => {
-=======
-arr.concat(arr2).forEach((item) => {
-    if(typeof(item)=== 'number') {
-        sum = sum + item
-    }
->>>>>>> e1095ca19ca34d706bcfc921fa3a3413297f990e
     // yedi item typeof 'number' ho vaney sum ma add garne
     if (typeof item === "number"){
         sum = item + sum
--- array/level11.js
+++ array/level11.js
@@ -9,7 +9,6 @@
 
 const arr = [-1,4,-5,1,-2,4]
 //Q1. calculate the sum of all -ve numbers in the array
-<<<<<<< HEAD
  const output = arr.reduce((sum,nextItem,nextid,orgArr)=>{
     if(nextItem < 0){
          sum = sum + nextItem
@@ -18,15 +17,6 @@ const arr = [-1,4,-5,1,-2,4]
  
  },0)
  console.log(output);
-=======
-const output = arr.reduce((sum,nextItem,nextid,orgArr)=>{
-    if(nextItem < 0){
-      sum = sum + nextItem
-    }
-    return sum
-},0)
-console.log(output)
->>>>>>> e1095ca19ca34d706bcfc921fa3a3413297f990e
 
 
 let sum = 0 
