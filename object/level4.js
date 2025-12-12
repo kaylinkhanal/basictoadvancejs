44c77ea2d23c1e6ebb68e52138d33b7238f65264
yogeshthapa-7 <yogsthapa@gmail.com>
2025-12-12 08:13:37 +0545

    object level4

--- object/level4.js
+++ object/level4.js
@@ -18,9 +18,15 @@ const addressDetails = {
 // Q1. output should ['GORKHA', 'NEPAL']
 
 // hint: first convert object to array using Object.values() method
+const newArr = Object.values(addressDetails.secondary);
+console.log(newArr);
 
 
 //Q2. output should be 'kaylin lives in los angeles, usa'
+const output =(user.name) + " lives in " + (addressDetails.primary.city) + "," + (addressDetails.primary.country);
+console.log(output);
 
 
 //Q3. output should be [[name, 'kaylin'], [age, 50]]
+const arr = Object.entries(user);
+console.log(arr);
