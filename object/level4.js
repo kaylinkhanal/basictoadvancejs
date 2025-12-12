4d8162fb0823abdb3c5eed1a22bab3c63fabc8f2
alishkdk <kdkalish4@gmail.com>
2025-12-12 08:08:08 +0545

    level 4 complete

--- object/level4.js
+++ object/level4.js
@@ -17,10 +17,17 @@ const addressDetails = {
 
 // Q1. output should ['GORKHA', 'NEPAL']
 
+let out=Object.values(addressDetails.secondary)
+console.log(out)
+
+
 // hint: first convert object to array using Object.values() method
 
 
 //Q2. output should be 'kaylin lives in los angeles, usa'
-
+let output = user.name + "lives in "+ addressDetails.primary.city+","+addressDetails.primary.country;
+console.log(output)
 
 //Q3. output should be [[name, 'kaylin'], [age, 50]]
+let output2=[Object.keys(user),Object.values(user)]
+console.log(output2)
