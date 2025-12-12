39b78cbb3c3fde1c9995748744bbe36f816060b3
Shyam Shah Thakuri <thakurishyam111@gmail.com>
2025-12-12 11:22:33 +0545

    solve

--- object/level4.js
+++ object/level4.js
@@ -16,11 +16,21 @@ const addressDetails = {
 }
 
 // Q1. output should ['GORKHA', 'NEPAL']
+let arr=Object.values(addressDetails.secondary)
+let arr10=arr.map((values)=>{
+    return values.toUpperCase()
+    
+})
+
+console.log(arr10)
 
 // hint: first convert object to array using Object.values() method
 
 
 //Q2. output should be 'kaylin lives in los angeles, usa'
+console.log(`${user.name} live in ${addressDetails.primary.city}, ${addressDetails.primary.country}`)
 
 
 //Q3. output should be [[name, 'kaylin'], [age, 50]]
+let arr5 =Object.entries(user)
+console.log(arr5)
