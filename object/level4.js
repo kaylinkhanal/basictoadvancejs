c0cc3e21d69b37fb24cff9199473c638665f3b76
aswinbaral2 <aswinbaral2@gmail.com>
2025-12-12 08:07:47 +0545

    task done

--- object/level4.js
+++ object/level4.js
@@ -1,7 +1,7 @@
 // HIGH LEVEL DIFFICULTY PROBLEM
 const user = {
     name: 'kaylin',
-    age:50
+    age: 50
 }
 
 const addressDetails = {
@@ -17,10 +17,12 @@ const addressDetails = {
 
 // Q1. output should ['GORKHA', 'NEPAL']
 
+console.log( Object.values(addressDetails.secondary));
 // hint: first convert object to array using Object.values() method
 
 
 //Q2. output should be 'kaylin lives in los angeles, usa'
-
+console.log(`Kaylin lives in ${addressDetails.primary.city}`)
 
 //Q3. output should be [[name, 'kaylin'], [age, 50]]
+console.log(Object.values(user), Object.keys(user));
