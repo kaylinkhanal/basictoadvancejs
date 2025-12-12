83171aacd8c2c2fa1778b78512e87ea8fc5bd3ae
Sona7x <sony.bajracharya22@gmail.com>
2025-12-12 08:04:13 +0545

    object level 3 solving

--- object/level3.js
+++ object/level3.js
@@ -1,36 +1,37 @@
 const generateRandomNumber = () => {
-    return Math.random()
-}
+  return Math.random();
+};
 
 const obj = {
-    level: 3,
-    subject: "opt math",
-    topics: ["calculus", "statistics", "computer"],
-    number:  generateRandomNumber()
-}
-
+  level: 3,
+  subject: "opt math",
+  topics: ["calculus", "statistics", "computer"],
+  number: generateRandomNumber(),
+};
 
 // Q1, if obj.random is more than 0.5, add +1 to the obj.level else -1 to obj.level
-
+if (generateRandomNumber() > 0.5) {
+  obj.level += 1;
+} else obj.level -= 1;
+console.log(obj.level);
+console.log(obj.number);
 
 // Q2. return only the topics name starting from letter 'c'
 // HINT
-const output = obj.topics.filter((item) => {
-    // yedi item ko first letter 'c' ho vaney matra return garne
-})
-console.log(output)
+const output = obj.topics.filter((item) => item.startsWith("c"));
+console.log(output);
 
 const obj2 = {
-    name: 'shiva',
-    address: {
-        city: 'kathmandu',
-        country: 'nepal'
-    },
-}
+  name: "shiva",
+  address: {
+    city: "kathmandu",
+    country: "nepal",
+  },
+};
 
-// combine obj and obj2 into obj3 
+// combine obj and obj2 into obj3
 
-//expected output : 
+//expected output :
 // {
 //     level: 3,
 //     subject: "opt math",
@@ -42,3 +43,6 @@ const obj2 = {
 //         country: 'nepal'
 //     },
 // }
+
+const obj3 = { ...obj, ...obj2 };
+console.log(obj3);
