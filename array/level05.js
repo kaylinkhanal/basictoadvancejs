dd88a08ae215abcaf7c0c1280f9c318b51b0f2e3
yogeshthapa-7 <yogsthapa@gmail.com>
2025-12-13 17:34:45 +0545

    resolved level05

--- array/level05.js
+++ array/level05.js
@@ -12,10 +12,6 @@ const arr = [5,1,4]
 
 //Q1. if item is divisible by 2, then multiply it by 3 else multiply it by 2
 // expected output: [10, 2, 12]
-<<<<<<< HEAD
-=======
-
->>>>>>> 7270b3306f949450a337e435cbcef1ba912ae010
 const mapped = arr.map((item) => {
     if(item % 2 ===0){
         return item * 3;
@@ -65,26 +61,3 @@ const names2 = names.filter((item) => {
     
 });
 console.log(names2);
-<<<<<<< HEAD
-=======
-
-// HINT: 
-const output = arr.map((item) => {
-    if(item % 2 === 0){
-        return item * 3
-    }else{
-        return item * 2
-    }
-})
-console.log(output); 
-
-//Q2. use filter to get only those items which are greater than 2
-// expected output: [5,4]
-const output2 = arr.filter((item) => {
-    if(item > 2){
-        return item 
-    }
-})
-console.log(output2); 
-
->>>>>>> 7270b3306f949450a337e435cbcef1ba912ae010
