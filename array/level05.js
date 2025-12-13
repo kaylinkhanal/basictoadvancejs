d5de7cadd021f78f77bcc29ff12afda5c2703db8
Yogesh Thapa <161129686+yogeshthapa-7@users.noreply.github.com>
2025-12-13 16:33:51 +0545

    updated level05

--- array/level05.js
+++ array/level05.js
@@ -12,7 +12,7 @@ const arr = [5,1,4]
 
 //Q1. if item is divisible by 2, then multiply it by 3 else multiply it by 2
 // expected output: [10, 2, 12]
-<<<<<<< HEAD
+
 const mapped = arr.map((item) => {
     if(item % 2 ===0){
         return item * 3;
@@ -62,7 +62,7 @@ const names2 = names.filter((item) => {
     
 });
 console.log(names2);
-=======
+
 // HINT: 
 const output = arr.map((item) => {
     if(item % 2 === 0){
@@ -81,4 +81,4 @@ const output2 = arr.filter((item) => {
     }
 })
 console.log(output2); 
->>>>>>> d80cd3902685af464c65d7788251b6eb3dc5c562
+
