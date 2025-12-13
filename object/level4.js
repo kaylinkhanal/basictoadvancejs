c0217525539bc3f68dbab9def6f3ffc0898cba78
hritikbk07 <hritikkami56@gmail.com>
2025-12-13 19:51:46 +0545

    complete level 5

--- object/level4.js
+++ object/level4.js
@@ -16,11 +16,20 @@ const addressDetails = {
 }
 
 // Q1. output should ['GORKHA', 'NEPAL']
+const allValues = Object.values(addressDetails.secondary).map(value => {
+  if (typeof value === 'string') {
+    return value.toUpperCase();
+  }
+  return value});
+console.log(allValues);
+
 
 // hint: first convert object to array using Object.values() method
 
 
 //Q2. output should be 'kaylin lives in los angeles, usa'
+console.log(`${user.name} lives in ${addressDetails.primary.city}, ${addressDetails.primary.country}'`);
 
 
 //Q3. output should be [[name, 'kaylin'], [age, 50]]
+console.log(Object.entries(user));
