279d80764d7b5dd66545cc52b65300831cf8df24
Birendra Bohara <bbirendra693@gmail.com>
2025-12-20 08:55:40 +0545

    Boolean level 1 qns solved

--- boolean/level1.js
+++ boolean/level1.js
@@ -9,3 +9,29 @@ console.log(10*a>=b) // true
 
 // Q3. check if b is divisible by a
 console.log(b%a===0) // true
+
+// Q4.	Check if 100 == "100".
+        console.log(100=="100"); // true
+
+// Q5.	Check if 100 === "100".
+        console.log(100==="100"); // false
+
+// Q6.	Check if true is equal to 1.
+        console.log(true==1); //  true
+        console.log(true===1); // false
+
+// Q7.	Check if false is equal to 0.
+        console.log(false==0); // true
+        console.log(false===0); //false
+
+// Q8.	Determine the Boolean value of "" (empty string).
+        console.log(Boolean("")); // false
+
+// Q9.	Determine the Boolean value of "Hello".
+        console.log(Boolean("Hello")); // true
+
+// Q10.	Determine the Boolean value of 0.
+        console.log(Boolean(0)); // false
+
+// Q11.	Determine the Boolean value of 1.
+        console.log(Boolean(1)); // true
