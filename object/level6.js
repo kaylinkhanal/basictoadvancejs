30e39150980abd106fae9b7d1d95253b10e5d154
Binu3030 <binut7874@gmail.com>
2025-12-21 22:19:02 +0545

    objleve6

--- object/level6.js
+++ object/level6.js
@@ -31,13 +31,32 @@ console.log(totalValue)
 const availableitem=cartItems.filter((x)=>x.price*x.quantity>600)
 console.log(availableitem)
 
+
+
+
+
 // 3. Convert the `cartItems` array into an **object**, where:
 
 //    * key → product name
 //    * value → total price of that product
 
+
+
+
+
+
+
+
+
 // 4. Find the **product object** with the **highest total price**
 //    (price × quantity).
+
+
+
+
+
+
+
 
 // 5. Create a **new array** where each object includes an additional property:
 
