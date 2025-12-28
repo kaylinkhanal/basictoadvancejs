2be35638a31b5b1117869df19079e698ba11f4b2
ashwinbaral2 <aswinbaral2@gmail.com>
2025-12-29 05:35:22 +0545

    adding new react app again

---
+++ ashwinfrontend/app/page.tsx
@@ -0,0 +1,21 @@
+// import Image from "next/image";
+
+// export default function Home() {
+//   return (
+//     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+//       
+//     </div>
+//   );
+// }
+
+import { url } from 'inspector'
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div>hey</div>
+  )
+}
+
+export default Home
