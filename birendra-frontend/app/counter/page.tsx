f5f45d6cfa2cc9140c6fcf04fdb949a0db751036
Birendra Bohara <bbirendra693@gmail.com>
2025-12-31 10:39:48 +0545

    Added counter app & like button

--- birendra-frontend/app/counter/page.tsx
+++ birendra-frontend/app/counter/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react'
 
 const Counter = () => {
-let [count, setCount] = useState(0)
+const [count, setCount] = useState(0)
 
 const increment = ()=>{
     setCount(count=>count+1)
---
+++ birendra-frontend/app/like/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { ThumbsUp } from 'lucide-react'
+import React, { useState } from 'react'
+
+const Like = () => {
+    const [liked, setLiked] = useState(false);
+    function handleLike(){
+        setLiked(liked? false:true)
+    }
+  return (
+    <div>
+        <button onClick={handleLike} className={`p-4 m-4 shadow-md ${liked? 'text-blue-700': 'text-gray-700'}`}>
+            <ThumbsUp/>Like
+            </button>
+    </div>
+  )
+}
+
+export default Like
