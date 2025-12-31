1f3d7f659d210f5dfeb3c23a78b35c6ff6a69187
Prakash Thapa <bluestar5pt4488@gmail.com>
2025-12-31 14:38:00 +0545

    error inspection

--- prakashfrontend/app/like/page.tsx
+++ prakashfrontend/app/like/page.tsx
@@ -9,11 +9,12 @@ const Like = () => {
     }
   return (
     <div>
-        <button onClick={handleLike} className={p-4 m-4 shadow-md ${liked? 'text-blue-700': 'text-gray-700'}}>
+        <button onClick={handleLike} 
+        className={`p-4 m-4 shadow-md ${liked? 'text-blue-700': 'text-gray-700'}`}>
             <ThumbsUp/>Like
             </button>
     </div>
-  )
+)
 }
 
 export default Like
